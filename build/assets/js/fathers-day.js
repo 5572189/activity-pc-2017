7d39772c84497b7b9a2d33f464fa5767ea8cc000
armdong <armdong@163.com>
2017-08-18 14:46:34 +0800

    remove the build directory from the repository

--- build/assets/js/fathers-day.js
+++
@@ -1,9 +0,0 @@
-(function(window, document) {
-	var oFlyBee = document.getElementById('hero'),
-	    movieclip1 = new JSMovieclip(oFlyBee, {
-	        height: 341,
-	        direction: 'v',
-	        frames_number: 2,
-	        framerate: 4
-	    }).play(true);
-})(window, document);
